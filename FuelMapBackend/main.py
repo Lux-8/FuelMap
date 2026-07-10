37c276e2cf380401a15421bd295c94adb1e38d0d
Lux-8 <lux89901@gmail.com>
2026-07-10 11:19:18 +0300

    Update API URL for production

--- FuelMapBackend/main.py
+++ FuelMapBackend/main.py
@@ -24,10 +24,7 @@ app.add_middleware(
     CORSMiddleware,
 
     allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500",
-        "http://127.0.0.1:5501",
-        "http://localhost:5501",
+        "https://fuelmap-production.up.railway.app",
     ],
 
     allow_credentials=True,
