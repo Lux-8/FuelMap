2065cedd710f14e96c7fbb951b5973c3b32f1352
Lux-8 <lux89901@gmail.com>
2026-07-10 12:43:17 +0300

    Update map.js

--- FuelMapFrontend/js/map.js
+++ FuelMapFrontend/js/map.js
@@ -18,20 +18,39 @@ function createMap(){
 
 }
 
-const secretMarker = L.marker([56.9057, 35.7215])
-    .addTo(map);
+function createMap(){
+
+    map = L.map("map", {
+        attributionControl:false
+    }).setView([56.8596,35.9119],8);
 
 
-secretMarker.bindPopup(`
-    <h3>🟣 FuelMap Secret Base</h3>
+    L.tileLayer(
+        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    ).addTo(map);
 
-    <p>
-    🎉 Вы нашли секретную точку FuelMap!
-    </p>
 
-    <p>
-    🚗 Первый исследователь Тверской области
-    </p>
+    markers = L.markerClusterGroup();
 
-    <b>🏆 Достижение получено</b>
-`);
+    map.addLayer(markers);
+
+
+    const secretMarker = L.marker([56.9057, 35.7215]);
+
+    secretMarker.addTo(map);
+
+
+    secretMarker.bindPopup(`
+        <h3>🟣 FuelMap Secret Base</h3>
+
+        <p>
+        🎉 Вы нашли секретную точку FuelMap!
+        </p>
+
+        <p>
+        🚗 Первый исследователь Тверской области
+        </p>
+
+        <b>🏆 Достижение получено</b>
+    `);
+}
