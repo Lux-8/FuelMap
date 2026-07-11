16190b232c4caa124386c7e67a95184d173799e7
Lux-8 <lux89901@gmail.com>
2026-07-11 21:38:28 +0300

    Revert "Add files via upload"
    This reverts commit bdb14ce6da6b34fb1541182fa7e6f8376c9c5a76.

--- utils.js
+++
@@ -1,7 +0,0 @@
-function getColor(status) {
-    switch (status) {
-        case "green":  return "#2ecc71";
-        case "orange": return "#f39c12";
-        default:       return "#95a5a6"; // gray — нет топлива ИЛИ данных о станции ещё нет
-    }
-}
