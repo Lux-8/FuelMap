246c979fd7c412383dc12e9fe0c10f234aff195b
Lux-8 <lux89901@gmail.com>
2026-07-23 12:28:25 +0300

    Add files via upload

--- FuelMapFrontend/js/map.js
+++ FuelMapFrontend/js/map.js
@@ -1,56 +1,27 @@
-function createMap(){
-
-    map = L.map("map",{
-
-        attributionControl:false
-
-    }).setView([56.8596,35.9119],8);
-
-    L.tileLayer(
-
-        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-
-    ).addTo(map);
-
-    markers=L.markerClusterGroup();
-
-    map.addLayer(markers);
-
-}
-
-function createMap(){
+function createMap() {
 
     map = L.map("map", {
-        attributionControl:false
-    }).setView([56.8596,35.9119],8);
-
+        attributionControl: false
+    }).setView([56.8596, 35.9119], 8);
 
     L.tileLayer(
-        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
+        {
+            subdomains: "abcd",
+            maxZoom: 20,
+            attribution: "&copy; OpenStreetMap &copy; CARTO"
+        }
     ).addTo(map);
-
 
     markers = L.markerClusterGroup();
 
     map.addLayer(markers);
 
-//Этот секретный маркер добавим позже
-    //const secretMarker = L.marker([56.9057, 35.7215]);
+    map.on("click", (e) => {
+        const { lat, lng } = e.latlng;
 
-    //secretMarker.addTo(map);
+        console.log("lat:", lat);
+        console.log("lng:", lng);
+    });
 
-
-    //secretMarker.bindPopup(`
-        //<h3>🟣 FuelMap Secret Base</h3>
-
-        //<p>
-        //🎉 Вы нашли секретную точку FuelMap!
-        //</p>
-
-        //<p>
-        //🚗 Первый исследователь Тверской области
-        //</p>
-
-        //<b>🏆 Достижение получено</b>
-    //`);
 }
